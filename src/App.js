d78e06c84d8cd0cbbfde8e8f5148497095af0da4
leekeifuture <opnoe09@gmail.com>
2020-07-05 14:18:49 +0300

    Add TelegramLoginButton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react'
-import './App.css'
-import logo from './logo.svg'
+import TelegramLoginButton from 'telegram-login-button'
+import styles from './App.module.css'
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+        <div className={styles.app}>
+            <header className={styles.appHeader}>
+                <TelegramLoginButton
+                    botName="ttschedule_bot"
+                    dataOnauth={user => console.log(user)}
+                    usePic
+                />
             </header>
         </div>
     )
